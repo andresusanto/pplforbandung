f795a5e59373c0ad8fe5717d88cd4211714e7c40
andresusanto <andre@andresusanto.info>
2015-03-31 13:18:50 +0700

    Base migration schema

---
+++ 6air/database/migrations/2015_03_31_041333_create_izin_airs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateIzinAirsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('izin_airs', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('nama');
+			
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('izin_airs');
+	}
+
+}
