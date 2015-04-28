a1e85b8d0bff1603e6b23f7b61c92de03d36524c
andresusanto <andre@andresusanto.info>
2015-04-28 10:23:33 +0700

    Bersih bersih (delete view lama)

--- 6air/resources/views/_OLD_TEMPLATE/HomepageDinas.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('dinas')
-
-@section('content')
-<div class="index"><h1><img src="{{asset('/img/logo-index.png')}}" ></h1><h2>Aplikasi Perizinan Air akan membantu anda dalam mengelola perizinan terkait air.</h2></div>
-			<ul class="social"></ul>
-
-@endsection
